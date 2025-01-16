a2f72a408fd186c0e6d1d85f4a23e19a7641477f
gxres042 <i@gxres.net>
2025-01-16 21:12:18 +0800

    feat: minify build

--- build.config.ts
+++ build.config.ts
@@ -4,6 +4,5 @@ export default defineBuildConfig({
   entries: ['src/index.ts'],
   outDir: 'dist',
   declaration: true,
-  externals: ['tinyglobby', 'fdir', 'picomatch'],
-  minify: true
+  externals: ['tinyglobby', 'fdir', 'picomatch']
 })
