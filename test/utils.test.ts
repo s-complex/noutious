e0a5a8c2d568335b146bce0609981e5400cf751e
Allenyou <i@allenyou.wang>
2025-01-19 15:55:04 +0800

    test: add unit test for processPostsData()

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,3 +1,4 @@
+import { stat } from 'node:fs/promises'
 import { describe, expect, it } from 'vitest'
 import { processPostsData, scan } from '../src/utils'
 
@@ -7,5 +8,39 @@ describe('markdown File Scanning', () => {
       'getting-started.md',
       'hello-world.md',
     ])
+  })
+})
+
+describe('processPostsData', () => {
+  it('should process markdown files', async () => {
+    expect(
+      await processPostsData([
+        'getting-started.md',
+        'hello-world.md',
+      ], './test/posts', '<!--more-->'),
+    ).toStrictEqual(
+      {
+        'getting-started': {
+          title: 'Getting started',
+          date: new Date('2025-01-10T00:00:00.000Z'),
+          source: 'test\\posts\\getting-started.md',
+          frontmatter: {},
+          updated: (await stat('./test/posts/getting-started.md')).mtime.toISOString(),
+          excerpt: '\nSome ways to use Noutious.\n\n',
+          tags: undefined,
+          categories: undefined,
+        },
+        'hello-world': {
+          title: 'Hello World',
+          date: new Date('2025-01-09T00:00:00.000Z'),
+          source: 'test\\posts\\hello-world.md',
+          frontmatter: {},
+          updated: (await stat('./test/posts/hello-world.md')).mtime.toISOString(),
+          excerpt: '\nWelcome to use Noutious!\n\n',
+          tags: undefined,
+          categories: undefined,
+        },
+      },
+    )
   })
 })
