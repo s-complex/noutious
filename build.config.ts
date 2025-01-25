fb0939adbb0af6ad44bd9033356014a431fc2bc6
gxres042 <i@gxres.net>
2025-01-25 17:56:57 +0800

    fix: add missing build.config.ts

---
+++ build.config.ts
@@ -0,0 +1,8 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+    entries: ['src/index.ts'],
+    outDir: 'dist',
+    declaration: true,
+    externals: ['tinyglobby', 'fdir', 'picomatch'],
+})
