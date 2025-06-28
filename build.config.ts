1a49f60570da03ce7100163afd3e76eb51f371b7
gxres042 <i@gxres.net>
2025-06-28 18:52:45 +0800

    chore: Separate bundle types

--- build.config.ts
+++ build.config.ts
@@ -4,11 +4,18 @@ export default defineBuildConfig({
 	entries: [
 		{
 			type: 'bundle',
-			input: './src/index.ts',
+			input: ['./src/index.ts'],
 			minify: true,
 			rolldown: {
 				external: ['consola', 'gray-matter', 'tinyglobby', 'pathe'],
 			},
 		},
+		{
+			type: 'bundle',
+			input: './src/types.ts',
+			dts: {
+				emitDtsOnly: true
+			}
+		}
 	],
 });
