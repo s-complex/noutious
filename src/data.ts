07051517005c0375d6323d795e4293fa77329dc3
gxres042 <i@gxres.net>
2025-12-28 21:10:54 +0800

    fix(queryData): use persistData.read() instead manual read persist data
    I just forget that I made a function for this...

--- src/data.ts
+++ src/data.ts
@@ -1,35 +1,20 @@
-import { readFile } from 'node:fs/promises';
-import { readConfig } from './utils/config';
 import { transformPosts, transformTaxonomies } from './utils/transform';
-import { existsSync } from 'node:fs';
-import type { Data, Post } from './types';
+import type { Post } from './types';
 import { scan } from './utils/scan';
-
-const DATA_PATH = `${readConfig().baseDir}/data.json`;
+import { persistData } from './persist';
 
 export async function queryData(
 	queryType: 'posts' | 'categories' | 'tags'
 ): Promise<Record<string, Post> | string[]> {
-	const isPersistDataExists = existsSync(DATA_PATH);
-	let persistData: Data | null = null;
-
-	if (isPersistDataExists) {
-		try {
-			const persistDataRaw = await readFile(DATA_PATH, 'utf-8');
-			persistData = JSON.parse(persistDataRaw);
-		} catch {
-			persistData = null;
-		}
-	}
-
+	const persist = await persistData.read()
 	const fileList = scan();
 
 	const taxonomies = transformTaxonomies(await fileList);
 
 	switch (queryType) {
 		case 'posts': {
-			if (persistData && persistData.posts && Object.keys(persistData.posts).length > 0) {
-				return persistData.posts;
+			if (persist && persist.posts && Object.keys(persist.posts).length > 0) {
+				return persist.posts;
 			}
 
 			return await transformPosts(await fileList);
@@ -37,19 +22,19 @@ export async function queryData(
 
 		case 'categories': {
 			if (
-				persistData &&
-				Array.isArray(persistData.categories) &&
-				persistData.categories.length > 0
+				persist &&
+				Array.isArray(persist.categories) &&
+				persist.categories.length > 0
 			) {
-				return persistData.categories;
+				return persist.categories;
 			}
 
 			return (await taxonomies).categories;
 		}
 
 		case 'tags': {
-			if (persistData && Array.isArray(persistData.tags) && persistData.tags.length > 0) {
-				return persistData.tags;
+			if (persist && Array.isArray(persist.tags) && persist.tags.length > 0) {
+				return persist.tags;
 			}
 
 			return (await taxonomies).tags;
